6f8878085510517f70142bab471f07155a191c08
Diana Kinyanjui <dianakinyanjui8@gmail.com>
2025-08-18 16:45:48 +0300

    Add files via upload
    week 1 submission

---
+++ index.py
@@ -0,0 +1,29 @@
+num1 = 23
+num2 = 12
+operation = "+"
+
+if operation == "+":
+    result = num1 + num2
+
+
+print(f"{num1} {operation} {num2} = {result}")
+
+operation = '*'
+
+if operation == "*":
+    result = num1 * num2
+    
+print(f"{num1} {operation} {num2} = {result}")
+
+operation = "-"
+
+if operation == "-":
+    result = num1 - num2
+    
+print(f"{num1} {operation} {num2} = {result}")
+    
+    
+    
+    
+
+    
